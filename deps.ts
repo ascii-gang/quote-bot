cf4cac8c3229d6c898f54770fd6c03a54e4bfe3b
user6879 <bpoliar01@gmail.com>
2023-02-13 23:15:57 +0500

    initial

---
+++ deps.ts
@@ -0,0 +1,4 @@
+export * from "https://deno.land/x/grammy@v1.13.1/mod.ts";
+export { serve } from "https://deno.land/std@0.163.0/http/server.ts";
+import "https://deno.land/x/dotenv@v3.2.0/load.ts";
+export { axiod } from "https://deno.land/x/axiod@0.26.2/mod.ts";
